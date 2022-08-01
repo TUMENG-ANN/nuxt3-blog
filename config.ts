0dc6a5e0c4264a50f879069377657fd4f55706a2
TUMENG-ANN <102861274+TUMENG-ANN@users.noreply.github.com>
2022-08-01 15:02:11 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,12 +1,12 @@
 export default {
-  title: "yun yu yuan", // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: "yunyuyuan", // github账户名
+  title: "现实与梦境", // 网站标题
+  nickName: "现与梦", // 昵称
+  githubName: "TUMENG-ANN", // github账户名
   githubRepo: "nuxt3-blog", // 勿修改
   githubBranch: "master", // 勿修改
   domain: "https://blog.halberd.cn", // rss域名
   SEO_description: "云与原的博客，书写值得书写的东西",
-  SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客",
+  SEO_keywords: "xianyumeng's blog,现与梦,现与梦的博客",
 
   themeColor: "#2aa0bb", // 主题色
   Comment: {
